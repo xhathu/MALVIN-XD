bead385cf60075ae430f197ae4d44217042039cc
xhathu <hathuhathu46@gmail.com>
2025-06-10 15:38:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "malvin~131EwRJB#CKrW5NU7d82K_Jn6ZcCxOyMd0iSoXq8Vvua30x4RS9w",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
